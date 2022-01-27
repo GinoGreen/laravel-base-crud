243ec533a4289b1de5fdcbb549f157cb64de798d
GinoGreen <luigiverde97@gmail.com>
2022-01-27 15:22:50 +0100

    init project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+
+   <main class="container">
+      <h1>Homepage</h1>
+   </main>
+   
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header class="container">
+   <ul class="nav">
+      <li class="nav-item">
+         <a class="nav-link active" aria-current="page" href="#">Fumetti</a>
+      </li>
+      <li class="nav-item">
+         <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+         <a class="nav-link" href="#">Link</a>
+      </li>
+   </ul>
+</header>
